0cba0b78a4b37c569501d6313947e1ac188c8c6c
caio <107592408+caio-cavalcante@users.noreply.github.com>
2024-09-17 14:40:28 -0300

    Paredes

---
+++ W1_3.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class W1_3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class W1_3 extends W1
+{
+    public W1_2 w1_2;
+    
+    public W1_3(W1_2 w1_2)
+    {
+        this.w1_2 = w1_2;
+        prepare();
+        super.jogador = getObjects(Jogador.class).get(0);
+    }
+    
+    public void act() {
+        if (super.jogador != null) {
+            if (super.jogador.getX() <= 20) {
+            Greenfoot.setWorld(w1_2);
+            super.jogador.setLocation(super.jogador.getX() + 10, super.jogador.getY());
+        }
+        
+        super.mostrarVida();
+        super.tomarDano();
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Grama grama = new Grama(700, 150);
+        addObject(grama,230,422);
+        grama.setLocation(300,575);
+        Jogador jogador = new Jogador();
+        addObject(jogador,24,471);
+        Cobra cobra = new Cobra(250);
+        addObject(cobra,550,464);
+        Grama grama2 = new Grama(15, 600);
+        addObject(grama2,423,232);
+        grama2.setLocation(602,296);
+    }
+}
